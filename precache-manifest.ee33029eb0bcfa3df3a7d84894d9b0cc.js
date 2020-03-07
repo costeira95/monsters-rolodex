e1b560227f24b27a1769c37df62f994d3aea2e24
Daniel Costeira <costeiradaniel@gmail.com>
2020-03-07 01:29:34 +0000

    Updates

---
+++ precache-manifest.ee33029eb0bcfa3df3a7d84894d9b0cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1e759e5c6b1d0305216c51a427335e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "02464aac441ec1f984b3",
+    "url": "/monsters-rolodex/static/css/main.df7443c6.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02464aac441ec1f984b3",
+    "url": "/monsters-rolodex/static/js/main.c3c11e05.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
